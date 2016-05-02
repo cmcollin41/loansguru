f963b6d935f591a1211bc561f361f79345238a1c
grandmasterspoc <dilloncortez@gmail.com>
2016-05-02 17:49:56 -0600

    http post working

--- app/models/loan_application.rb
+++ app/models/loan_application.rb
@@ -3,12 +3,8 @@ class LoanApplication < ActiveRecord::Base
   after_create :format_names
 
   def post_to_site
-    uri = URI.parse "https://olps.macfarlanegp.com/leadpost.aspx"
-    request = Net::HTTP::Post.new uri.path
-    request.body = application_xml_packet
-    request.content_type = 'text/xml'
-    response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
-    response.body
+    uri = "https://olps.macfarlanegp.com/leadpost.aspx"
+    request = HTTParty.post uri, :body => application_xml_packet, :headers => {'Content-type' => 'text/xml'}
   end  
 
   def application_xml_packet
