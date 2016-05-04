981d803874933a9c8b333f409ebfca35cfa90cf7
grandmasterspoc <dilloncortez@gmail.com>
2016-05-04 15:04:51 -0600

    changed log_level to info

--- app/controllers/loan_applications_controller.rb
+++ app/controllers/loan_applications_controller.rb
@@ -1,26 +1,26 @@
-class LoanApplicationsController < ApplicationController
-  
-  def new
-    @loan_application = LoanApplication.new
+  class LoanApplicationsController < ApplicationController
+    
+    def new
+      @loan_application = LoanApplication.new
+    end
+
+    def create
+      loan_application = LoanApplication.new(application_params)
+      loan_application.application_ip = get_ip_address
+      if loan_application.save
+        redirect_to root_path
+      else
+        render :new
+      end    
+    end
+    
+    private  
+
+    def get_ip_address
+      request.remote_ip
+    end  
+
+    def application_params
+      params.require(:loan_application).permit(:amount, :zip_code, :street_address, :city, :state, :phone_number, :application_ip, :first_name, :last_name, :email_address)
+    end  
   end
-
-  def create
-    loan_application = LoanApplication.new(application_params)
-    loan_application.application_ip = get_ip_address
-    if loan_application.save
-      redirect_to root_path
-    else
-      render :new
-    end    
-  end
-  
-  private  
-
-  def get_ip_address
-    request.remote_ip
-  end  
-
-  def application_params
-    params.require(:loan_application).permit(:amount, :zip_code, :street_address, :city, :state, :phone_number, :application_ip, :first_name, :last_name, :email_address)
-  end  
-end
